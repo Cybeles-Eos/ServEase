f77d7204b6321ca9279d9522750fe5a19634d607
Dionysus <158821418+Cybeles-Eos@users.noreply.github.com>
2025-12-31 17:49:09 +0800

    setup: ServEase project

---
+++ resources/views/front/layouts/sections/header.blade.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="head-top">
+
+    </div>
+    <div class="header-main m-padding">
+        <div class="h-con-links">
+            <img src="{{ asset('images/header-logo.svg') }}" alt="">
+            <div class="h-con-links__divider"></div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact</a></li>
+                <li>
+                    <a href="#">
+                        <svg width="14" height="17" viewBox="0 0 15 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0.75 5.625C0.75 3.32725 0.75 2.17756 1.46419 1.46419C2.17756 0.75 3.32725 0.75 5.625 0.75H8.875C11.1728 0.75 12.3224 0.75 13.0358 1.46419C13.75 2.17756 13.75 3.32725 13.75 5.625V12.125C13.75 14.4228 13.75 15.5724 13.0358 16.2858C12.3224 17 11.1728 17 8.875 17H5.625C3.32725 17 2.17756 17 1.46419 16.2858C0.75 15.5724 0.75 14.4228 0.75 12.125V5.625Z" stroke="currentColor" stroke-width="1.5"/>
+                            <path d="M13.6671 12.125H3.91712C3.1615 12.125 2.78369 12.125 2.47331 12.2079C2.06 12.3187 1.68315 12.5364 1.38064 12.839C1.07814 13.1417 0.860634 13.5186 0.75 13.932" stroke="currentColor" stroke-width="1"/>
+                            <path d="M4 4.8125H10.5M4 7.65625H8.0625" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+                        </svg>
+                        Local Services
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="h-cta-btns">
+            <a href="#" class="login--btn">Login</a>
+            <a href="#" class="btn btn--tertiary">Sign up</a>
+        </div>
+    </div>
+</header>
---
+++ resources/views/front/pages/custom-pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('front.layouts.base')
+
+@section('content')
+    <main class="main-page page--home">
+        <section class="section--hero">
+            <h1>Hero</h1>
+        </section>
+    </main>
+@endsection
