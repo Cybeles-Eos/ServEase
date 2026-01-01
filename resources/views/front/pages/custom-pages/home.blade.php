c067b26c535271cfb29eeb9fc1896682ba10353d
Dionysus <158821418+Cybeles-Eos@users.noreply.github.com>
2026-01-01 16:59:28 +0800

    save

--- resources/views/front/pages/custom-pages/home.blade.php
+++ resources/views/front/pages/custom-pages/home.blade.php
@@ -3,9 +3,15 @@
 @section('content')
     <main class="main-page page--home">
         <section class="section--hero m-padding">
-            <h1>
-                ds
-            </h1>
+            <h1>Find reliable local experts fast with our smart service hub</h1>
+            <p>A smart platform that connects people who need help with locals who can get the job done anytime.</p>
+
+            <div class="section--hero__inp">
+                <input type="text" placeholder="What service are you looking for?" >
+                <button>Search</button>
+            </div>
+            
+
         </section>
         <section style="width: 100%; height: 100vh">
 
